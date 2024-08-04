d29174d757fe2b1cdc5ed9f9b25d60e7999c4fdf
Jeremy Fisher <jeremy.adams.fisher@gmail.com>
2024-08-04 14:51:51 -0700

    more debugging docker build, add vastai training start script

--- vastai-train.sh
+++ vastai-train.sh
@@ -1,5 +1,8 @@
-# TODO: issue this command through a tunnel
+#!/usr/bin/env bash
 
-# nohup python -O -m slow_diffusion.cli fit \
-#     --config /Users/jeremiahfisher/Code/slow_diffusion/configs/fashion_mnist.yaml \
-#     &> train.log &
+set -e -o pipefail
+
+export INSTANCE_ID=$1
+export CONFIG=$2
+
+vastai execute $INSTANCE_ID "nohup python -O -m slow_diffusion.cli fit --config slow_diffusion/configs/$CONFIG.yaml &> train.log &"
