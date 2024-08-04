c609c43ccb76df0948c5fddaeb9ddacf515781fb
Jeremy Fisher <jeremy.adams.fisher@gmail.com>
2024-08-04 15:26:44 -0700

    fix import issues for CLI

--- vastai-create-instance.sh
+++ vastai-create-instance.sh
@@ -12,7 +12,7 @@ fi
 
 vastai create instance $INSTANCE_ID \
     --image jeremyadamsfisher1123/slow-diffusion \
-    --env "-e WANDB_API_KEY=$(cat .secrets.json | jq -r .WANDB_API_KEY) -e GOOGLE_APPLICATION_CREDENTIALS_B64=$(base64 -w 0 < ./service_account.json)" \
+    --env "-e WANDB_API_KEY=$(cat .secrets.json | jq -r .WANDB_API_KEY) -e GOOGLE_APPLICATION_CREDENTIALS_B64=$(base64 < ./service_account.json)" \
     --disk 100 \
     --ssh \
     --onstart ./vastai-on-start.sh
