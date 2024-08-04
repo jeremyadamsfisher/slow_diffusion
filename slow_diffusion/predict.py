66a8a41f0d6755dc62384f1d99a85875cf6ada4d
Jeremy Fisher <jeremy.adams.fisher@gmail.com>
2024-08-04 15:05:00 -0700

    yet more debugging

--- slow_diffusion/predict.py
+++ slow_diffusion/predict.py
@@ -1,9 +1,10 @@
 """Placeholder for prediction, required to use Cog"""
 from cog import BasePredictor
+from typing import Any
 
 class Predictor(BasePredictor):
     def setup(self):
         ...
 
-    def predict(self):
+    def predict(self) -> Any:
         ...
