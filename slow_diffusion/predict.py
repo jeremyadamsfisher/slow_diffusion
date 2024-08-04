0811955df3d3b186dee97b8a46f15a6b876b09e9
Jeremy Fisher <jeremy.adams.fisher@gmail.com>
2024-08-04 14:59:44 -0700

    docker build requires a predict file

---
+++ slow_diffusion/predict.py
@@ -0,0 +1,9 @@
+"""Placeholder for prediction, required to use Cog"""
+from cog import BasePredictor
+
+class Predictor(BasePredictor):
+    def setup(self):
+        ...
+
+    def predict(self):
+        ...
