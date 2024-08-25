284032df864ea664806a3f7b11b4309f60f10f65
Jeremy Fisher <jeremy.adams.fisher@gmail.com>
2024-08-25 09:38:02 -0700

    fix gcloud must be installed before setup

--- vastai-on-start.sh
+++ vastai-on-start.sh
@@ -4,6 +4,13 @@ set -euxo pipefail
 
 ## Set up dev environment
 
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.asc] http://packages.cloud.google.com/apt cloud-sdk main" \
+| tee -a /etc/apt/sources.list.d/google-cloud-sdk.list \
+&& curl https://packages.cloud.google.com/apt/doc/apt-key.gpg \
+| tee /usr/share/keyrings/cloud.google.asc \
+&& apt-get update -y \
+&& apt-get install google-cloud-sdk -y
+
 mkdir /slow_diffusion
 echo $GOOGLE_APPLICATION_CREDENTIALS_B64 \
     | base64 --decode \
@@ -13,15 +20,7 @@ git clone https://github.com/jeremyadamsfisher/slow_diffusion.git /slow_diffusio
 gcloud auth activate-service-account --key-file=$GOOGLE_APPLICATION_CREDENTIALS
 cd /slow_diffusion/slow_diffusion
 
-echo "deb [signed-by=/usr/share/keyrings/cloud.google.asc] http://packages.cloud.google.com/apt cloud-sdk main" \
-| tee -a /etc/apt/sources.list.d/google-cloud-sdk.list \
-&& curl https://packages.cloud.google.com/apt/doc/apt-key.gpg \
-| tee /usr/share/keyrings/cloud.google.asc \
-&& apt-get update -y \
-&& apt-get install google-cloud-sdk -y
-
 ## Add creds for later, if desired
-
 echo WANDB_API_KEY=$WANDB_API_KEY >> /etc/environment
 echo GOOGLE_APPLICATION_CREDENTIALS_B64=$GOOGLE_APPLICATION_CREDENTIALS_B64 >> /etc/environment
 echo GOOGLE_APPLICATION_CREDENTIALS=/slow_diffusion/service_account.json >> /etc/environment
